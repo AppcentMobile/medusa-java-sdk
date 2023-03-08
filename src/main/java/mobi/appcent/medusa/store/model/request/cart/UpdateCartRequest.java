93e57af8531507bf82ffd6b8e2862c5847d06721
Eren <eren.alparslan@appcent.mobi>
2023-03-08 22:32:57 +0300

    feat(cart-api): Refactor carts api

---
+++ src/main/java/mobi/appcent/medusa/store/model/request/cart/UpdateCartRequest.java
@@ -0,0 +1,63 @@
+package mobi.appcent.medusa.store.model.request.cart;
+
+import com.squareup.okhttp.Call;
+import mobi.appcent.medusa.store.*;
+import mobi.appcent.medusa.store.common.HeaderConstant;
+import mobi.appcent.medusa.store.common.HttpMethod;
+import mobi.appcent.medusa.store.common.UrlConstant;
+import mobi.appcent.medusa.store.model.request.BaseRequest;
+import mobi.appcent.medusa.store.model.response.StoreCartsRes;
+import mobi.appcent.medusa.store.model.response.StorePostCartReq;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by erenalpaslan on 8.03.2023
+ */
+public class UpdateCartRequest extends BaseRequest<StoreCartsRes> {
+
+    private final MedusaSdkClient client;
+    private final String id;
+    private StorePostCartReq body;
+
+    public UpdateCartRequest(MedusaSdkClient client, String id) {
+        this.client = client;
+        this.id = id;
+    }
+
+    public UpdateCartRequest body(StorePostCartReq body) {
+        this.body = body;
+        return this;
+    }
+
+    @Override
+    protected Call buildCall() throws ApiException {
+        // create path and map variables
+        String path = UrlConstant.CART_PATH + "/" + id;
+        Map<String, String> headers = new HashMap<>();
+
+        final String[] localVarAccepts = { HeaderConstant.APPLICATION_JSON };
+        final String localVarAccept = client.selectHeaderAccept(localVarAccepts);
+
+        if (localVarAccept != null) headers.put(HeaderConstant.ACCEPT, localVarAccept);
+        headers.put(HeaderConstant.CONTENT_TYPE, HeaderConstant.APPLICATION_JSON);
+
+        return client.buildCall(path, HttpMethod.POST, null, null, body, headers, null, null);
+    }
+
+    @Override
+    public ApiResponse<StoreCartsRes> execute() throws ApiException {
+        Call call = buildCall();
+        return client.execute(call);
+    }
+
+    @Override
+    public void executeAsync(ApiCallback<StoreCartsRes> callback) throws ApiException {
+        Call call = buildCall();
+        client.executeAsync(call, callback);
+    }
+}
