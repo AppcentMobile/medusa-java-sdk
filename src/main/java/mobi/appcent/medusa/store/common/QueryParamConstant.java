90ab43f669f2a4b9550bddda00a39e70e6d630cc
Eren <eren.alparslan@appcent.mobi>
2023-03-09 22:20:22 +0300

    feat(order-api): Refactor orders api

--- src/main/java/mobi/appcent/medusa/store/common/QueryParamConstant.java
+++ src/main/java/mobi/appcent/medusa/store/common/QueryParamConstant.java
@@ -8,4 +8,9 @@ public class QueryParamConstant {
     public static final String LIMIT = "limit";
     public static final String CREATED_AT = "created_at";
     public static final String UPDATED_AT = "updated_at";
+    public static final String DISPLAY_ID = "display_id";
+    public static final String FIELDS = "fields";
+    public static final String EXPAND = "expand";
+    public static final String EMAIL = "email";
+    public static final String SHIPPING_ADDRESS = "shipping_address";
 }
